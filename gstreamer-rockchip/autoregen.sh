131f4e79375952989ee8609a8f5dd79363a55d73
YunJin.Jiang <yunjin.jiang@advantech.com.cn>
2020-06-12 10:17:53 +0800

    add missing files

---
+++ gstreamer-rockchip/autoregen.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+./autogen.sh --srcdir=/home/aka/work/code/rk3399/debian/rk3399_risc/external/gstreamer-rockchip --prefix=/usr --libdir=/usr/lib/aarch64-linux-gnu --target=aarch64-linux-gnu --host=aarch64-linux-gnu --program-prefix= --disable-valgrind --disable-examples --disable-rkximage $@
